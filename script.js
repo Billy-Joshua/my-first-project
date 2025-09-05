b97ae65451c8e96c898e911e3e0ab269be17535f
Billy19092024 <billyjoshuaishimwe@gmail.com>
2025-09-05 18:07:28 +0200

    fix title

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
+function showDate() {
+  document.getElementById('demo').innerHTML = Date();
+}
 
 function myFunction() {
-  var x = document.getElementsByClassName("cancer");
-  for (var i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  var elements = document.getElementsByClassName('cancer');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = 'none';
   }
 }
-function showDate() {
-  document.getElementById("demo").innerHTML = new Date();
-}
-function light(sw) {
-  var pic;
-  if (sw === 0) {
-    pic = "image/pic_bulboff.gif";
-  } else if (sw===1) { 
-    pic = "image/pic_bulbon.gif"; 
-  } else { 
-  pic ="image/pic-bulbcolar.gif"
-  } 
 
-  document.getElementById("myImage").src = pic;
-  
+function light(state) {
+  var image = document.getElementById('myImage');
+  if (state == 1) {
+    image.src = 'image/pic_bulbon.gif';
+  } else {
+    image.src = 'image/pic_bulboff.gif';
+  }
 }
 
   
